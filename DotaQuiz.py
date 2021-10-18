c51c40567c298dfaf12b8887e36cf2d1bb980a2c
n1nekto <ad.moxito@mail.ru>
2021-10-18 10:55:32 +0300

    first part

---
+++ DotaQuiz.py
@@ -0,0 +1,9 @@
+player_level = 0;
+
+lvl1 = ["Team Secret", "Team Spirit", "OG", "PSG.LGD"] #first questions
+lvl2 = ["Miposhka", "Mira", "Collapse", "TORONTOTOKYO", "Yatoro"] #second questions
+lvl3 = ["Dark Willow", "Lion", "Phantom Assasin", "Magnus"] #third questions
+lvl4 = [""] #fourt questions
+lvl5 = [""] #fifth questions
+
+question = [lvl1, lvl2, lvl3, lvl4, lvl5]
